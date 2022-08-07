3e8c52d6432a463b19cd547faabbcd78d28dcc7a
Joshua Darnell <josh@kurotek.com>
2022-08-07 14:12:53 -0700

    #25: Write to S3 bucket

--- orgs-exporter/index.js
+++ orgs-exporter/index.js
@@ -1,12 +1,30 @@
 'use strict';
 
 const { fetchOrgsAndEndorsements } = require('./data-access');
+const { writeDataToS3 } = require('./utils');
+
+const ORG_RESULTS_BUCKET_NAME = 'reso-public',
+  ORG_RESULTS_FILE_NAME = 'OrgsAndEndorsements.json';
 
 exports.handler = async event => {
+  const data = await fetchOrgsAndEndorsements();
+
+  const serializedData = JSON.stringify({
+    Description: 'RESO Organizations and Endorsements',
+    GeneratedOn: new Date().toISOString(),
+    Data: data
+  });
+
+  await writeDataToS3({
+    bucketName: ORG_RESULTS_BUCKET_NAME,
+    fileName: ORG_RESULTS_FILE_NAME,
+    serializedData
+  });
+
   try {
     return {
       statusCode: 200,
-      body: await fetchOrgsAndEndorsements()
+      body: { orgCount: data?.length }
     };
   } catch (err) {
     console.error(`ERROR: ${err}, event: ${event}`);
---
+++ orgs-exporter/utils.js
@@ -0,0 +1,19 @@
+const AWS = require('aws-sdk');
+
+const { AWS_REGION } = process.env;
+
+const writeDataToS3 = async ({ bucketName, fileName, serializedData } = {}) => {
+  const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: AWS_REGION });
+  const params = {
+    Bucket: bucketName,
+    Key: fileName,
+    Body: serializedData
+  };
+  const result = await s3.putObject(params).promise();
+
+  console.log('result is: ' + JSON.stringify(result));
+};
+
+module.exports = {
+  writeDataToS3
+};
