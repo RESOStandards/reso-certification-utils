b0b09111e57531ac1b1490252f8ac6376445a970
Joshua Darnell <josh@kurotek.com>
2022-08-07 12:31:19 -0700

    #25: fixed linter errors

--- orgs-exporter/data-access.js
+++ orgs-exporter/data-access.js
@@ -1,68 +1,67 @@
-const https = require("https");
+const https = require('https');
 
-const REQUEST_TIMEOUT_MS = process.env.REQUEST_TIMEOUT_S * 1000;
+const REQUEST_TIMEOUT_MS = (process.env.REQUEST_TIMEOUT_S || 30) * 1000;
 const { API_KEY, SERVER_URL, ENDORSEMENTS_PATH, UOI_GOOGLE_SHEET_URL } = process.env;
 
-const DATA_DICTIONARY_DISPLAY_NAME = "Data Dictionary",
-  DATA_DICTIONARY_IDX_PAYLOAD_DISPLAY_NAME = "Data Dictionary with IDX Payload",
-  WEB_API_CORE_DISPLAY_NAME = "Web API Core";
+const DATA_DICTIONARY_DISPLAY_NAME = 'Data Dictionary',
+  DATA_DICTIONARY_IDX_PAYLOAD_DISPLAY_NAME = 'Data Dictionary with IDX Payload',
+  WEB_API_CORE_DISPLAY_NAME = 'Web API Core';
 
-const CURRENT_DATA_DICTIONARY_VERSIONS = ["1.7"],
-  CURRENT_WEB_API_CORE_VERSIONS = ["2.0.0"],
-  ORGANIZATION_COLUMN = "OrganizationStatus",
-  ACTIVE_STATUS_FLAG = "1",
-  MAPPED_ENDORSEMENTS_FIELD_NAME = "Endorsements";
+const ORGANIZATION_COLUMN = 'OrganizationStatus',
+  ACTIVE_STATUS_FLAG = '1',
+  // CURRENT_DATA_DICTIONARY_VERSIONS = ['1.7'],
+  // CURRENT_WEB_API_CORE_VERSIONS = ['2.0.0'],
+  MAPPED_ENDORSEMENTS_FIELD_NAME = 'Endorsements';
 
 const buildEndorsementsFilterOptions = (from = 0) => {
   return {
     options: {
       from,
       endorsementFilter: [],
-      statusFilter: ["passed", "revoked", "certified", "recipient_notified"],
+      statusFilter: ['passed', 'revoked', 'certified', 'recipient_notified'],
       showMyResults: false,
       providerUoi: null,
-      searchKey: "",
-      sortBy: "asc",
-    },
+      searchKey: '',
+      sortBy: 'asc'
+    }
   };
 };
 
-const isOrganizationActive = record => record[ORGANIZATION_COLUMN] && record[ORGANIZATION_COLUMN] === ACTIVE_STATUS_FLAG;
+const isOrganizationActive = record =>
+  record[ORGANIZATION_COLUMN] && record[ORGANIZATION_COLUMN] === ACTIVE_STATUS_FLAG;
 
-const getEndorsementDisplayName = (status) => {
-  if (status === "passed" || status === "recipient_notified") return "Passed";
-  if (status === "certified") return "Certified";
+const getEndorsementDisplayName = status => {
+  if (status === 'passed' || status === 'recipient_notified') return 'Passed';
+  if (status === 'certified') return 'Certified';
   return status;
 };
 
-const getEndorsementTypeDisplayName = (type) => {
-  if (type === "web_api_server_core") return WEB_API_CORE_DISPLAY_NAME;
-  if (type === "data_dictionary") return DATA_DICTIONARY_DISPLAY_NAME;
-  if (type === "data_dictionary_with_IDX_payload")
-    return DATA_DICTIONARY_IDX_PAYLOAD_DISPLAY_NAME;
+const getEndorsementTypeDisplayName = type => {
+  if (type === 'web_api_server_core') return WEB_API_CORE_DISPLAY_NAME;
+  if (type === 'data_dictionary') return DATA_DICTIONARY_DISPLAY_NAME;
+  if (type === 'data_dictionary_with_IDX_payload') return DATA_DICTIONARY_IDX_PAYLOAD_DISPLAY_NAME;
   return `${type}`;
 };
 
 const getOrgs = async () => await get(UOI_GOOGLE_SHEET_URL);
 
 const covertGoogleSheetJsonToOrgsJson = ({ values = [] } = {}) => {
-
   const ORGS_COLUMNS = [
-    "OrganizationUniqueId",
-    "OrganizationType",
-    "AssnToMls",
-    "OrganizationName",
-    "OrganizationAddress1",
-    "OrganizationCity",
-    "OrganizationStateOrProvince",
-    "OrganizationPostalCode",
-    "OrganizationWebsite",
-    "OrganizationCountry",
-    "ModificationTimestamp",
-    "OrganizationLatitude",
-    "OrganizationLongitude",
-    "OrganizationMemberCount",
-    "OrganizationCertName"
+    'OrganizationUniqueId',
+    'OrganizationType',
+    'AssnToMls',
+    'OrganizationName',
+    'OrganizationAddress1',
+    'OrganizationCity',
+    'OrganizationStateOrProvince',
+    'OrganizationPostalCode',
+    'OrganizationWebsite',
+    'OrganizationCountry',
+    'ModificationTimestamp',
+    'OrganizationLatitude',
+    'OrganizationLongitude',
+    'OrganizationMemberCount',
+    'OrganizationCertName'
   ];
 
   const keys = values[0] || [],
@@ -75,10 +74,12 @@ const covertGoogleSheetJsonToOrgsJson = ({ values = [] } = {}) => {
     }, {});
 
     if (isOrganizationActive(transformed)) {
-      results.push(ORGS_COLUMNS.reduce((acc, columnName) => {
-        acc[columnName] = transformed[columnName] || null;
-        return acc;
-      }, {}));
+      results.push(
+        ORGS_COLUMNS.reduce((acc, columnName) => {
+          acc[columnName] = transformed[columnName] || null;
+          return acc;
+        }, {})
+      );
     }
     return results;
   }, []);
@@ -107,19 +108,18 @@ const fetchEndorsements = async () => {
 
     Object.entries(reportsByOrgs).map(([uoi, endorsements]) => {
       if (!results[uoi]) results[uoi] = [];
-      results[uoi].push(endorsements.map(
-        ({ type, version, status, providerUoi, statusUpdatedAt }) => {
+      results[uoi].push(
+        endorsements.map(({ type, version, status, providerUoi, statusUpdatedAt }) => {
           return {
-            endorsement: getEndorsementTypeDisplayName(type),
-            version,
-            status: getEndorsementDisplayName(status),
-            providerUoi,
-            statusUpdatedAt,
+            Endorsement: getEndorsementTypeDisplayName(type),
+            Version: version,
+            Status: getEndorsementDisplayName(status),
+            ProviderUoi: providerUoi,
+            StatusUpdatedAt: statusUpdatedAt
           };
-        }
-      ));
+        })
+      );
     });
-
   } while (lastStatusCode >= 200 && lastStatusCode < 300);
 
   return results;
@@ -140,33 +140,33 @@ const fetchOrgsAndEndorsements = async () => {
 
 const post = async (url, body = {}) => {
   const options = {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      Authorization: `ApiKey ${API_KEY}`,
+      'Content-Type': 'application/json',
+      Authorization: `ApiKey ${API_KEY}`
     },
     maxRedirects: 5,
+    timeout: REQUEST_TIMEOUT_MS
   };
 
   return new Promise((resolve, reject) => {
-    const req = https.request(url, options, (res) => {
-      let rawData = "";
+    const req = https.request(url, options, res => {
+      let rawData = '';
 
-      res.on("data", (chunk) => {
+      res.on('data', chunk => {
         rawData += chunk;
       });
 
-      res.on("end", () => {
+      res.on('end', () => {
         try {
           resolve({ statusCode: res?.statusCode, data: JSON.parse(rawData) });
-        }
-        catch (err) {
+        } catch (err) {
           reject(new Error(err));
         }
       });
     });
 
-    req.on("error", (err) => {
+    req.on('error', err => {
       reject(new Error(err));
     });
 
@@ -175,26 +175,25 @@ const post = async (url, body = {}) => {
   });
 };
 
-const get = async (url) => {
+const get = async url => {
   return new Promise((resolve, reject) => {
-    const req = https.get(url, (res) => {
-      let rawData = "";
+    const req = https.get(url, res => {
+      let rawData = '';
 
-      res.on("data", (chunk) => {
+      res.on('data', chunk => {
         rawData += chunk;
       });
 
-      res.on("end", () => {
+      res.on('end', () => {
         try {
           resolve(JSON.parse(rawData));
-        }
-        catch (err) {
+        } catch (err) {
           reject(new Error(err));
         }
       });
     });
 
-    req.on("error", (err) => {
+    req.on('error', err => {
       reject(new Error(err));
     });
   });
@@ -203,4 +202,3 @@ const get = async (url) => {
 module.exports = {
   fetchOrgsAndEndorsements
 };
-
---
+++ orgs-exporter/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { fetchOrgsAndEndorsements } = require('./data-access');
+
+exports.handler = async event => {
+  try {
+    return {
+      statusCode: 200,
+      body: await fetchOrgsAndEndorsements()
+    };
+  } catch (err) {
+    console.error(`ERROR: ${err}, event: ${event}`);
+    return {
+      statusCode: 400,
+      body: JSON.stringify('ERROR fetching endorsements!')
+    };
+  }
+};
