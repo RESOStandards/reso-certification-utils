66a044f352a2d1efebf0b65f7fbfad3fc98b6573
Joshua Darnell <darnjo@users.noreply.github.com>
2022-08-22 19:17:18 -0700

    #25 Create s3 Orgs and Endorsements exporter (#26)
    * #25: Initial check-in
    
    * #25: fixed linter errors
    
    * #25: Write to S3 bucket
    
    * #25: Added S3 Dump of Production Orgs and Endorsements
    
    * #25: Remove CodeQL analysis as it needs an update
    
    * #25: Remove CodeQL analysis as it needs an update
    
    * #25: Added sleep function to throttle requests on the server
    
    * #25: Fixed formatting issues
    
    * #25: Added ContentType header and changed sleep interval
    
    * #25: Added Stats and normalized coordinates and member counts

---
+++ orgs-exporter/utils.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const { AWS_REGION } = process.env;
+
+const writeDataToS3 = async ({
+  bucketName,
+  fileName,
+  serializedData,
+  contentType = 'application/json; charset=utf-8'
+} = {}) => {
+  const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: AWS_REGION });
+  const params = {
+    Bucket: bucketName,
+    Key: fileName,
+    Body: serializedData,
+    ContentType: contentType
+  };
+
+  await s3.putObject(params).promise();
+};
+
+const sleep = async (ms = 500) => new Promise(resolve => setTimeout(resolve, ms));
+
+module.exports = {
+  writeDataToS3,
+  sleep
+};
