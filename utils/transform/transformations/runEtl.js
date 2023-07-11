8a98bb02be8acd625061a688c7ce0882baed607f
mohit-s96 <mohitsrivastava67@gmail.com>
2023-07-11 16:18:36 +0530

    #51: extract transformations into separate files

---
+++ utils/transform/transformations/runEtl.js
@@ -0,0 +1,51 @@
+const {
+  processDataAvailability: { processDataAvailability }
+} = require('reso-certification-etl');
+const runEtlOnReport = async data => {
+  try {
+    // recover the raw availability report
+    const {
+      description = 'RESO Data Availability Report',
+      version,
+      generatedOn,
+      resources,
+      fields,
+      lookupValues,
+      lookups,
+      optInStatus
+    } = data;
+
+    const rawAvailabilityReport = {
+      resources,
+      fields: fields.map(({ resourceName, fieldName, frequency }) => ({
+        resourceName,
+        fieldName,
+        frequency
+      })),
+      lookupValues: lookupValues.map(({ resourceName, fieldName, lookupValue, frequency }) => ({
+        resourceName,
+        fieldName,
+        lookupValue,
+        frequency
+      })),
+      lookups
+    };
+
+    const rescoredReport = await processDataAvailability(rawAvailabilityReport);
+
+    return {
+      ...rescoredReport,
+      description,
+      version,
+      generatedOn,
+      optInStatus
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+module.exports = {
+  runEtlOnReport
+};
