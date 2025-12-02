c0f1f15a00906d6ca21a190e689b15c4a926eae2
darnjo <josh@reso.org>
2025-12-02 10:50:26 -0800

    v3.0.0: Fixed missing outputPath in replicate

--- index.js
+++ index.js
@@ -111,6 +111,7 @@ if (require?.main === module) {
         strictMode: parseBooleanValue(strictMode),
         maxPageSize: parseInt(maxPageSize) ?? undefined,
         top: parseInt(top) ?? undefined,
+        outputPath,
         shouldSaveResults: !!outputPath
       };
 
